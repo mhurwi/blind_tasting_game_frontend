e59f2aedcd4b54f4ef2c50ee1890de99fed628f6
Mark Hurwitz <mhurwitzus@gmail.com>
2014-11-26 21:20:57 -0600

    implement super basic anonymous user authentication (no email, no password, just tracking by session)

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('login');
   this.route('games', { path: "/"}, function(){
   });
   this.route('games.new', { path: "/new"})
--- app/routes/games.js
+++ app/routes/games.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('game');
+  },
+  actions: {
+    logout: function() {
+      this.get('session.auth').logout();
+    }
   }
 });
